e975cfa488f1c902a9b6a2ec04b20cb1e3d43a66
lethi <volethi30062003@gmail.com>
2025-05-23 22:00:15 +0700

    initial base project

--- Backend/Library_Management/DataLayer/Entities/ProjectPrn232Context.cs
+++ Backend/Library_Management/DataLayer/Entities/ProjectPrn232Context.cs
@@ -10,10 +10,7 @@ public partial class ProjectPrn232Context : DbContext
     {
     }
 
-    public ProjectPrn232Context(DbContextOptions<ProjectPrn232Context> options)
-        : base(options)
-    {
-    }
+    public ProjectPrn232Context(DbContextOptions<ProjectPrn232Context> options) : base(options) { }
 
     public virtual DbSet<Author> Authors { get; set; }
 
@@ -40,10 +37,6 @@ public partial class ProjectPrn232Context : DbContext
     public virtual DbSet<Role> Roles { get; set; }
 
     public virtual DbSet<User> Users { get; set; }
-
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=localhost;Database=Project_PRN232;Trusted_Connection=SSPI;Encrypt=false;TrustServerCertificate=true;");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
---
+++ Backend/Library_Management/DataLayer/Repositories/Abstraction/IPaginationRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer.Repositories.Abstraction
+{
+    public interface IPaginationRepository<T> where T : class
+    {
+        Task<PaginationResult<T>> GetPaginatedAsync(int pageNumber = 1, int pageSize = 10, Expression<Func<T, bool>>? filter = null, Func<IQueryable<T>, IQueryable<T>>? includes = null);
+    }
+}
