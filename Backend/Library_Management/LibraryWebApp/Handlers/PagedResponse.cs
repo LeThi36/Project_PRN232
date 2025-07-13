56a77cf3dfb04c96dfb2c93eb45a66d469f02f8a
Huynhnmt <nghuynh5079@gmail.com>
2025-07-13 12:18:04 +0700

    Add feature book success

---
+++ Backend/Library_Management/LibraryWebApp/Handlers/PagedResponse.cs
@@ -0,0 +1,14 @@
+﻿namespace LibraryWebApp.Handlers
+{
+    public class PagedResponse<T>
+    {
+        public List<T> Data { get; set; } = new();
+        public int TotalCount { get; set; }
+        public int TotalPages { get; set; }
+        public int PageSize { get; set; }
+        public int PageNumber { get; set; }
+        public bool HasPreviousPage { get; set; }
+        public bool HasNextPage { get; set; }
+    }
+
+}
