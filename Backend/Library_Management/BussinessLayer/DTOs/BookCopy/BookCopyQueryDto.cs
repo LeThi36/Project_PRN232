6c7caff6eb2e71a020bb7d1ec60f5dfaa930318c
tutien2904 <tutien29042003@gmail.com>
2025-07-28 19:52:52 +0700

    UI manage book copy

---
+++ Backend/Library_Management/BussinessLayer/DTOs/BookCopy/BookCopyQueryDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BussinessLayer.DTOs.BookCopy
+{
+    public class BookCopyQueryDto
+    {
+        public string? CopyCode { get; set; }
+        public string? Status { get; set; }
+        public int Page { get; set; } = 1;
+        public int PageSize { get; set; } = 5;
+    }
+}
